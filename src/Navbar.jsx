d0d6b578d9a2d68f629de270b13d343f60a28048
Himanshu Yadav <himanshu.yadav@aknamed.com>
2023-08-08 23:56:55 +0530

    Initial setup

---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div> HIMANSHU </div>
+            <div>
+                <div> About </div>
+                <div> Experience </div>
+                <div> Education </div>
+                <div> Projects </div>
+                <div> Contacts </div>
+            </div>
+        </div>
+    )
+};
+
+export default Navbar;
+
